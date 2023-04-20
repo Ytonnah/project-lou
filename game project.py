8e2f60533991124fb092cee03a9bb0359dc37848
Durid <62970031+Ytonnah@users.noreply.github.com>
2023-04-20 11:43:02 +0800

    Update game project.py

--- game project.py
+++ game project.py
@@ -1,42 +1,65 @@
 #project lou
 import pygame as pg
 
+
+screen = pg.display.set_mode((640, 480))
+
 class obstacle:
-    def create_rect(x=0,y=0):
-        return pg.Rect(300,300,x,y)
+    def __init__(self,screen,x,y):
+       obs1 = pg.Rect(300,300,x,y)
+       #self.create_rect(screen,"white",obs1)
+       pg.draw.rect(screen,"white",obs1)
+       pg.display.flip()
+
+    def create_rect(x=0,y=0,screen=screen):
+        obs1=  pg.Rect(300,300,x,y)
+        return pg.draw.rect(screen,'red',obs1 )
+    
         
 
 def main():
 
-    screen = pg.display.set_mode((640, 480))
+    
     clock = pg.time.Clock()
     rect = pg.Rect(300, 220, 20, 20)
     velocity = (0, 0)
     rect2 = pg.Rect(300,230, 500,10)
     
     done = False
-    gravity = True
+    gravity = False
+
+
     #creating the map using arrays
 
     map_data = {
         "WWWWWWWWWWWWWWWW",
         "W      W   W   W",
         "W       W  W   W",
-        "W"
+        "W",
+        "WWWWWWWWWWWWWWWW"
     }
+
+    for rows in map_data:
+        print("\n")
+        for col in rows:
+            print("x",sep="")
+       
+
     rowx = 0
     rowy = 0
     for rows in map_data:
         rowy+=10
         for cols in rows:
             rowx+=10
+            #print("x",end="")
             if cols == "W":
-                pg.draw.rect(screen,'blue',obstacle.create_rect(rowx,rowy)
+                
+                obstacle(screen,rowx,rowy)
+                #obstacle.create_rect(rowx,rowy,screen)
+                #pg.draw.rect(screen,'blue',obstacle.create_rect(rowx,rowy)
+            #print("\n")
 
-                )
-
-
-
+                
 
     #game loop
     while not done:
@@ -50,7 +73,9 @@ def main():
         if keys[pg.K_d]: #to move right
             rect.x += 4
         if keys[pg.K_w]:
-            rect.y -= 4
+            
+            #rect.y -= 4
+            rect.y -=4
         if keys[pg.K_s]:
             rect.y +=4
 
@@ -58,13 +83,17 @@ def main():
             falling = True
             if keys[pg.K_w]:
                 falling = False
-                rect.y-4
+                for i in range(4):
+                    rect.y-=15*(i*0.8)
+
             if falling:
-                rect.y+=8
+                for i in range(4):
+                    rect.y+=15*(i*0.2)#0.8 is the pull strength
+                #rect.y+=15
 
         screen.fill((40, 40, 40))
         pg.draw.rect(screen, (150, 200, 20), rect)
-        pg.draw.rect(screen, 'red',rect2)
+        #pg.draw.rect(screen, 'red',rect2)
 
         pg.display.flip()
         clock.tick(30)
